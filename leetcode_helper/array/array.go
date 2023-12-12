a28df91f1b04bb67fd8eaf98a4c11c3f0795e76d
Lifeissimple-zxc <mararkarp@gmail.com>
2023-12-13 00:57:01 +0100

    Introduced changes to pass more test cases

--- leetcode_helper/array/array.go
+++ leetcode_helper/array/array.go
@@ -4,21 +4,22 @@ import "fmt"
 
 // SimpleMaxProduct is a naive implementation of maxProduct algo
 func SimpleMaxProduct(nums []int) int {
-	// store max indexes here
-	var maxIx1, maxIx2 int
+	// max1 & max2 store max values
+	// maxIx stores indeces to max values in the array
+	var max1, max2, maxIx1, maxIx2 int
 	for cur1, cur2 := 0, -1; cur2 < len(nums); {
 		// first max
-		if nums[cur1] > nums[maxIx1] {
+		if nums[cur1] > max1 {
+			max1 = nums[cur1]
 			maxIx1 = cur1
 		}
 		// move first cursor if possible
 		if cur1+1 <= len(nums)-1 {
 			cur1++
 		}
-
-		// test case [10, 2 ,5 , 2] fails
-		// Reason nums[cur2] > nums[maxIx2] is never true in this scenario
-		if cur2 >= 0 && nums[cur2] > nums[maxIx2] && cur2 != maxIx1 {
+		// [2,2,1,8,1,5,4,5,2,10,3,6,5,2,3] fails now
+		if cur2 >= 0 && nums[cur2] > max2 && cur2 != maxIx1 {
+			max2 = nums[cur2]
 			maxIx2 = cur2
 		}
 		cur2++
@@ -27,3 +28,5 @@ func SimpleMaxProduct(nums []int) int {
 	return (nums[maxIx1] - 1) * (nums[maxIx2] - 1)
 
 }
+
+// TODO implement a simpler approach, it
