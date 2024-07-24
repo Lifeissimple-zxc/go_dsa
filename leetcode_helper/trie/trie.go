1f57e847ca98e6c376a5be64accb210e3634da7e
Lifeissimple-zxc <mararkarp@gmail.com>
2024-07-25 00:15:10 +0200

    started testing search suggestions system

--- leetcode_helper/trie/trie.go
+++ leetcode_helper/trie/trie.go
@@ -12,6 +12,11 @@ type Node struct {
 	isEnd    bool
 }
 
+// GetChildren is a getter for children
+func (n *Node) GetChildren() [alphabetSize]*Node {
+	return n.children
+}
+
 // Trie represents a Trie
 type Trie struct {
 	root *Node
@@ -57,3 +62,63 @@ func (t *Trie) Search(w string) bool {
 	}
 	return cursor.isEnd
 }
+
+func (t *Trie) prefix(prfx string) *Node {
+	wLen := len(prfx)
+	cursor := t.root
+	for i := 0; i < wLen; i++ {
+		// offsetting with 'a' based on ascii tables
+		charIx := (strings.ToLower(prfx)[i]) - 'a'
+		// empty char --> time to append
+		if cursor.children[charIx] == nil {
+			return nil
+		}
+		// update cursor for traversal
+		cursor = cursor.children[charIx]
+	}
+	return cursor
+}
+
+// FindWords locates n words based on prfx
+func (t *Trie) FindWords(prfx string, n int) []string {
+	res := make([]string, 0, n)
+	// find node where prefix ends
+	prfxNode := t.prefix(prfx)
+	if prfxNode == nil {
+		// no prefix node means there is no words
+		return nil
+	}
+	if prfxNode.isEnd {
+		res = append(res, prfx)
+	}
+
+	var recurse func(node *Node, prfx string)
+	recurse = func(node *Node, prfx string) {
+		for i, c := range node.children {
+			if c == nil {
+				continue
+			}
+			if len(res) == n {
+				return
+			}
+
+			prfx += string(i + 'a')
+			// word found
+			if c.isEnd {
+				res = append(res, prfx)
+			}
+
+			// backtracking-like logic
+			if len(res) < n {
+				recurse(c, prfx)
+			}
+			prfx = prfx[:len(prfx)-1]
+		}
+	}
+
+	recurse(prfxNode, prfx)
+	return res
+}
+
+// TODO func that returns a 2d slice with suggestions for each char from input:
+// https://leetcode.com/problems/search-suggestions-system/description/?envType=study-plan-v2&envId=leetcode-75
